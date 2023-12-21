8615365576a39302ec7262186ccd9fc3c6ef3ad3
Kennidy Stillwell <you@example.com>
2023-12-21 11:07:31 -0500

    added comments

--- app.js
+++ app.js
@@ -1,49 +1,70 @@
+//importing necessary modules
 const express = require('express');
 const axios = require('axios');
 
+//create instance of express application
 const app = express();
+//the server will listen on port 3000
 const port = 3000;
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+//handle requests to root URL ('/')
 app.get('/', (req, res) => {
+  //send the 'index.html' file when accessing the root URL
   res.sendFile(__dirname + '/index.html');
 });
 
+//handle requests to the '/search' endpoint
 app.get('/search', async (req, res) => {
   try {
+    //API key for movie database
     const apiKey = 'ac401c464aa25311d2eca44e26fccc5c';
+    //gets the movie title from the query parameters
     const userInput = req.query.movieTitle;
 
+    //make an asynchronous request to TMDb's search endpoint
     const response = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${userInput}`
     );
 
+    //extracts the list of movies from the response data
     const movies = response.data.results;
+    //sends the list of movies as a JSON response
     res.json(movies);
   } catch (error) {
+    //handle errors by logging them and sending a 500 Internal Server Error response
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
+//handles requests to the '/similar/:movieId' endpoint
 app.get('/similar/:movieId', async (req, res) => {
   try {
+    //API key for movie database
     const apiKey = 'ac401c464aa25311d2eca44e26fccc5c';
+    //gets the movie ID from the route parameters
     const movieId = req.params.movieId;
 
+    //makes an asynchronous request to TMDb's similar movies endpoint
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}`
     );
 
+    //extracts the list of similar movies from the response data
     const similarMovies = response.data.results;
+    //send the list of similar movies as a JSON response
     res.json(similarMovies);
   } catch (error) {
+    //handle errors by logging them and sending a 500 Internal Server Error response
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
+//starts the express server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
--- public/app.js
+++ public/app.js
@@ -1,49 +1,69 @@
+//waits for the HTML document to be fully loaded before executing the code
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('searchForm').addEventListener('submit', function (event) {
-      event.preventDefault();
-      const movieTitle = document.getElementById('movieTitle').value;
-  
-      const resultsList = document.getElementById('resultsList');
-      resultsList.innerHTML = '';
-  
-      fetch(`/search?movieTitle=${encodeURIComponent(movieTitle)}`)
+  //attachs an event listener to the 'searchForm' when it is submitted
+  document.getElementById('searchForm').addEventListener('submit', function (event) {
+    //prevents the default form submission behavior
+    event.preventDefault();
+
+    //gets the movie title entered by the user
+    const movieTitle = document.getElementById('movieTitle').value;
+
+    //gets the element where search results will be displayed
+    const resultsList = document.getElementById('resultsList');
+    //clear any existing content in the results list
+    resultsList.innerHTML = '';
+
+    //fetches movie search results from the server using the entered movie title
+    fetch(`/search?movieTitle=${encodeURIComponent(movieTitle)}`)
+      .then(response => response.json())
+      .then(movies => {
+        //iterate through the retrieved movies and display them in the results list
+        movies.forEach(movie => {
+          const listItem = document.createElement('li');
+          const link = document.createElement('a');
+          link.href = '#';
+          link.dataset.movieid = movie.id;
+          link.textContent = movie.title;
+          listItem.appendChild(link);
+          resultsList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        //handles errors by logging them to the console
+        console.error(error);
+      });
+  });
+
+  //attaches an event listener to the 'resultsList' when a link is clicked
+  document.getElementById('resultsList').addEventListener('click', function (event) {
+    //prevents the default link click behavior
+    event.preventDefault();
+
+    //checks if the clicked element is a link (anchor)
+    if (event.target.tagName === 'A') {
+      //gets the movie ID associated with the clicked link
+      const movieId = event.target.dataset.movieid;
+
+      //fetches similar movies from the server based on the selected movie ID
+      fetch(`/similar/${movieId}`)
         .then(response => response.json())
-        .then(movies => {
-          movies.forEach(movie => {
+        .then(similarMovies => {
+          //gets the element where search results will be displayed
+          const resultsList = document.getElementById('resultsList');
+          //clears any existing content in the results list
+          resultsList.innerHTML = '';
+
+          //iterates through the retrieved similar movies and display them in the results list
+          similarMovies.forEach(similarMovie => {
             const listItem = document.createElement('li');
-            const link = document.createElement('a');
-            link.href = '#';
-            link.dataset.movieid = movie.id;
-            link.textContent = movie.title;
-            listItem.appendChild(link);
+            listItem.textContent = similarMovie.title;
             resultsList.appendChild(listItem);
           });
         })
         .catch(error => {
+          //handles errors by logging them to the console
           console.error(error);
         });
-    });
-  
-    document.getElementById('resultsList').addEventListener('click', function (event) {
-      event.preventDefault();
-      if (event.target.tagName === 'A') {
-        const movieId = event.target.dataset.movieid;
-  
-        fetch(`/similar/${movieId}`)
-          .then(response => response.json())
-          .then(similarMovies => {
-            const resultsList = document.getElementById('resultsList');
-            resultsList.innerHTML = '';
-  
-            similarMovies.forEach(similarMovie => {
-              const listItem = document.createElement('li');
-              listItem.textContent = similarMovie.title;
-              resultsList.appendChild(listItem);
-            });
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    });
+    }
   });
+});
